5c9456ae4c9a0c22e1e77da3b6045312eb7543a0
ArynYT <49909132+ArynYT@users.noreply.github.com>
2019-04-23 19:13:53 +0600

    Create test7.py

---
+++ test7.py
@@ -0,0 +1,38 @@
+from colorama import init
+from colorama import Fore, Back, Style
+
+init()
+
+print( Fore.WHITE )
+print( Back.YELLOW )
+
+print("I LOVE PYTHON ")
+
+print( Back.GREEN )
+print("THE BEST COMPUTER LANGUAGE!!(love.) ")
+
+print( Back.BLACK )
+
+what = input("DO YOU LOVE PYTHON? ((WRITE yes or no)!!!!) ")
+
+if what == 'yes' :
+	print( Back.YELLOW )
+	print("AND I LOVE PYTHON ")
+elif what == 'no' :
+	asd = input("WHY? ")
+	print( Back.YELLOW )
+	print("I UNDERSTAND ")
+
+print( Back.CYAN )
+
+print("COMMAND DID WITН LOVE BY ARYN! (6V CLASS!)")
+
+print("COMMAND ENDED!")
+
+print("THANK YOU FOR USING!")
+
+print("SOURCE CODE: https://github.com/ArynYT/ILovePython/blob/master/test7.py")
+
+print("PRESS [ENTER] TO GO OUT!")
+
+input()
